38c8cffb57f97ae593e8bf0ebc0723dda87b2099
Jasir Ullah Khan <jasirullah12345@gmail.com>
2023-03-21 22:26:57 +0500

    added search input to datatable!

--- src/components/datatable/index.js
+++ src/components/datatable/index.js
@@ -1,25 +1,35 @@
-import React, {useMemo} from 'react';
-import InputField from "../InputField";
+import React, {useMemo, useState} from 'react';
 import Table from "./table";
 import Pagination from "./pagination";
+import SearchIconDark from "assets/icons/Search=Dark.svg";
 
 const Datatable = (props) => {
     const {
         searchText, setSearchText, headers, rows, totalRecords, recordPerPage, setRecordPerPage, page, setPage
     } = props;
+    const [search, setSearch] = useState(searchText);
 
     const totalPages = useMemo(() => {
         return Math.ceil(totalRecords / recordPerPage);
     }, [totalRecords, recordPerPage])
 
+    const onKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            setSearchText(search);
+        }
+    }
+
     return (<div className={'flex flex-col items-end'}>
-        <div className={'w-full max-w-[287px]'}>
-            <InputField
-                hideDetails
-                name={'search'}
-                placeholder={'Search'}
-                value={searchText}
-                onChange={(e) => setSearchText(e.target.value)}/>
+        <div className={'w-full max-w-[287px] relative'}>
+            <input type="text"
+                   name={'search'}
+                   className={'w-full bg-[#F0F0F0] text-[#72767B] font-normal text-base py-[12px] pl-[16px] pr-[50px] rounded-[5px] outline-none'}
+                   placeholder={'Search'}
+                   value={search}
+                   onChange={(e) => setSearch(e.target.value)}
+                   onKeyPress={onKeyPress}/>
+            <img src={SearchIconDark} alt="" onClick={() => setSearchText(search)}
+                 className={'absolute right-[16px] top-1/2 -translate-y-1/2 cursor-pointer'}/>
         </div>
         <div className={'w-full my-8'}>
             <Table headers={headers} rows={rows}/>
--- src/pages/users/list.js
+++ src/pages/users/list.js
@@ -17,105 +17,42 @@ const UserList = () => {
     const fetchData = async () => {
         // const response = await axios.get('/users');
         // const data = await response.json();
-        const data = [
-            {
-                id: 1,
-                name: 'John Doe',
-                age: 25,
-                veteran: true,
-                children: [
-                    {
-                        id: 3,
-                        name: 'John Doe',
-                        age: 25,
-                        veteran: true,
-                        children: [
-                            {
-                                id: 51,
-                                name: 'John Doe',
-                                age: 25,
-                                veteran: true
-                            },
-                            {
-                                id: 52,
-                                name: 'John Doe',
-                                age: 25,
-                                veteran: true
-                            }
-                        ]
-                    },
-                    {
-                        id: 4,
-                        name: 'John Doe',
-                        age: 25,
-                        veteran: true,
-                        children: [
-                            {
-                                id: 5,
-                                name: 'John Doe',
-                                age: 25,
-                                veteran: true
-                            },
-                            {
-                                id: 6,
-                                name: 'John Doe',
-                                age: 25,
-                                veteran: true
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                id: 2,
-                name: 'Doe',
-                age: 28,
-                veteran: false,
-                children: [
-                    {
-                        id: 7,
-                        name: 'John Doe',
-                        age: 25,
-                        veteran: true
-                    },
-                    {
-                        id: 8,
-                        name: 'John Doe',
-                        age: 25,
-                        veteran: true,
-                        children: [
-                            {
-                                id: 9,
-                                name: 'John Doe',
-                                age: 25,
-                                veteran: true
-                            },
-                            {
-                                id: 10,
-                                name: 'John Doe',
-                                age: 25,
-                                veteran: true
-                            }
-                        ]
-                    }
-                ]
-            },
-            {
-                id: 11,
-                name: 'John Doe',
-                age: 25,
-                veteran: true
-            }
-        ]
+        const data = [{
+            id: 1, name: 'John Doe', age: 25, veteran: true, children: [{
+                id: 3, name: 'John Doe', age: 25, veteran: true, children: [{
+                    id: 51, name: 'John Doe', age: 25, veteran: true
+                }, {
+                    id: 52, name: 'John Doe', age: 25, veteran: true
+                }]
+            }, {
+                id: 4, name: 'John Doe', age: 25, veteran: true, children: [{
+                    id: 5, name: 'John Doe', age: 25, veteran: true
+                }, {
+                    id: 6, name: 'John Doe', age: 25, veteran: true
+                }]
+            }]
+        }, {
+            id: 2, name: 'Doe', age: 28, veteran: false, children: [{
+                id: 7, name: 'John Doe', age: 25, veteran: true
+            }, {
+                id: 8, name: 'John Doe', age: 25, veteran: true, children: [{
+                    id: 9, name: 'John Doe', age: 25, veteran: true
+                }, {
+                    id: 10, name: 'John Doe', age: 25, veteran: true
+                }]
+            }]
+        }, {
+            id: 11, name: 'John Doe', age: 25, veteran: true
+        }]
         setTotalRecords(50);
         setRows(data);
     }
 
-    return (
-        <div className={'py-[14px] px-[30px]'}>
-            <Datatable searchText={searchText} setSearchText={setSearchText} headers={headers} rows={rows} totalRecords={totalRecords} recordPerPage={recordPerPage} setRecordPerPage={setRecordPerPage} page={page} setPage={setPage}/>
-        </div>
-    );
+    return (<div className={'py-[14px] px-[30px]'}>
+            <Datatable searchText={searchText} setSearchText={setSearchText} headers={headers} rows={rows}
+                       totalRecords={totalRecords} recordPerPage={recordPerPage} setRecordPerPage={setRecordPerPage}
+                       page={page} setPage={setPage}/>
+        </div>);
 };
 
 export default UserList;
